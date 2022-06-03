c63d4f7e63f6620d0f4c453d7fc84fc7eeca1f33
iSSAtAKAHASHI <105526762+iSSAtAKAHASHI@users.noreply.github.com>
2022-06-03 14:56:27 +0900

    Update WheelMotor_model1.py

--- WheelMotor_model1.py
+++ WheelMotor_model1.py
@@ -12,13 +12,15 @@ BIN2 = 33
 
 #GPIOのモード
 GPIO.setmode(GPIO.BOARD)#物理ピン番号でGPIOを指定
-GPIO.setup(AIN1,GPIO.OUT)#←ここでエラーoutがだめ？→out×,,OUT〇
+GPIO.setup(AIN1,GPIO.OUT)#←ここでエラーoutがだめ？→Result:out×,,OUT〇
 GPIO.setup(AIN2,GPIO.OUT)
+
 #周波数設定
 a1 = GPIO.PWM(AIN1,255)#255Hz
 a2 = GPIO.PWM(AIN2,255)#255Hz
 b1 = GPIO.PWM(BIN1,255)
 b2 = GPIO.PWM(BIN2,255)
+
 #PWM起動
 a1.start(0)#Aenable接続（E）
 a2.start(0)#Aphase接続（P）
@@ -26,7 +28,8 @@ b1.start(0)
 b2.start(0)
 
 duty = 25 #duty比　回転速度変更用変数
-#DDRV8355 = MODE0 
+#DDRV8355 = MODE0
+
 #右モータ関数
 def right_forward():#E=1 P=0の時
     a1.ChangeDutyCycle(duty)
@@ -40,6 +43,7 @@ def right_back():#E=1 P=1の時
 def right_stop():#E=0 P=0?の時
     a1.ChangeDutyCycle(0)
     a2.ChangeDutyCycle(0)
+    
 #左モーター関数
 def left_forward():
     b1.ChangeDutyCycle(duty)
@@ -54,7 +58,7 @@ def left_stop():
     b2.ChangeDutyCycle(0)
 
 while True:
-    x = int(input('kaiten'))#→何回かのデータから平均をとるべき→外れ値の除外目的
+    x = int(input('kaiten'))#→考え：何回かのデータから平均をとるべき→外れ値の除外目的
 
     if 200 < x < 400:#前進
         right_forward()
