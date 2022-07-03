32552934d599ce3418d3608e1e13321e3e208dfa
ShimadaKeigo <86919312+ShimadaKeigo@users.noreply.github.com>
2022-07-03 16:16:39 +0900

    Update serial_write_sample.py

--- sensor_data/TWE-Lite/serial_write_sample.py
+++ sensor_data/TWE-Lite/serial_write_sample.py
@@ -1,8 +1,13 @@
 import serial
 
 s = serial.Serial("/dev/serial0",115200,timeout=10) #serial通信の設定、"serialポート設定,通信速度,待つ時間(初期設定は∞,0にすると全く待たない．)"
-s.write(b'hello world')#バイナリ通信のとき，"b"を入れる
-s.close()#最後にcloseすればいい
+while True:
+    if s.in_waitng > 0:
+        resv_data = s.read(1)
+        print(type(recv_data))
+        a = struct.unpack_from("B",recv_data ,0)
+        print(a)
+        print("success!!") #finish sending
 
 #参考
 #http://www.s12600.net/psy/python/21-2.html
