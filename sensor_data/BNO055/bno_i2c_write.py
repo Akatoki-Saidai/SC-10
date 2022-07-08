65ce3ad30828fb2c7412fed0ad821021cd24352d
takashin9 <91837495+takashin9@users.noreply.github.com>
2022-07-08 17:21:39 +0900

    Update bno_i2c_write.py

--- sensor_data/BNO055/bno_i2c_write.py
+++ sensor_data/BNO055/bno_i2c_write.py
@@ -44,46 +44,47 @@ print('Magnetometer ID:    0x{0:02X}'.format(mag))
 print('Gyroscope ID:       0x{0:02X}\n'.format(gyro))
 
 print('Reading BNO055 data, press Ctrl-C to quit...')
-while True:
-    # Read the Euler angles for heading, roll, pitch (all in degrees).
-    heading, roll, pitch = bno.read_euler()
-    # Read the calibration status, 0=uncalibrated and 3=fully calibrated.
-    sys, gyro, accel, mag = bno.get_calibration_status()
-    # Print everything out.
-    print('Heading={0:0.2F} Roll={1:0.2F} Pitch={2:0.2F}\tSys_cal={3} Gyro_cal={4} Accel_cal={5} Mag_cal={6}'.format(
-          heading, roll, pitch, sys, gyro, accel, mag))
-    # Other values you can optionally read:
-    # Orientation as a quaternion:
-    #x,y,z,w = bno.read_quaterion()
-    # Sensor temperature in degrees Celsius:
-    #temp_c = bno.read_temp()
-    # Magnetometer data (in micro-Teslas):
-    mx,my,mz = bno.read_magnetometer()
-    # Gyroscope data (in degrees per second):
-    Gx,Gy,Gz = bno.read_gyroscope()
-    # Accelerometer data (in meters per second squared):
-    ax,ay,az = bno.read_accelerometer()
-    # Linear acceleration data (i.e. acceleration from movement, not gravity--
-    # returned in meters per second squared):
-    # x,y,z = bno.read_linear_acceleration()
-    # Gravity acceleration data (i.e. acceleration just from gravity--returned
-    # in meters per second squared):
-    #x,y,z = bno.read_gravity()
-    # Sleep for a second until the next reading.
-    print ("magx=",mx,"magy="my,"magz=",mz,"Gyx=",Gx,"Gyy=",Gy,"Gyz=",Gz,"accelx=",ax,"accely=",ay,"accelz=",az) 
-    time.sleep(1)
-    now = datetime.datetime.now()
-recordtime = 'rec_{0:%Y%m%d}'.format(now)
-print("recordtime = %s" % recordtime)
-
-data = "magx="+str(mx)+"magy="+str(my)+"magz="+str(mz)+"Gyx="+str(Gx)+"Gyy="+str(Gy)+"Gyz="+str(Gz)+"accelx="+str(ax)+"accely="+str(ay)+"accelz="+str(az)
-data = recordtime + "," + data
-data = data.split(',') 
-print("data= %s" % data)
-
 with open('log.csv', 'w') as f:   # log.csvのファイルに保存される
-    writer = csv.writer(f)
-    writer.writerow(data)
-f.close()
-print("loggging done. see log.csv")
-    #参考https://qiita.com/kmaepu/items/779ab8e45bfe96230224
+    while True:
+        # Read the Euler angles for heading, roll, pitch (all in degrees).
+        heading, roll, pitch = bno.read_euler()
+        # Read the calibration status, 0=uncalibrated and 3=fully calibrated.
+        sys, gyro, accel, mag = bno.get_calibration_status()
+        # Print everything out.
+        print('Heading={0:0.2F} Roll={1:0.2F} Pitch={2:0.2F}\tSys_cal={3} Gyro_cal={4} Accel_cal={5} Mag_cal={6}'.format(
+            heading, roll, pitch, sys, gyro, accel, mag))
+        # Other values you can optionally read:
+        # Orientation as a quaternion:
+        #x,y,z,w = bno.read_quaterion()
+        # Sensor temperature in degrees Celsius:
+        #temp_c = bno.read_temp()
+        # Magnetometer data (in micro-Teslas):
+        mx,my,mz = bno.read_magnetometer()
+        # Gyroscope data (in degrees per second):
+        Gx,Gy,Gz = bno.read_gyroscope()
+        # Accelerometer data (in meters per second squared):
+        ax,ay,az = bno.read_accelerometer()
+        # Linear acceleration data (i.e. acceleration from movement, not gravity--
+        # returned in meters per second squared):
+        # x,y,z = bno.read_linear_acceleration()
+        # Gravity acceleration data (i.e. acceleration just from gravity--returned
+        # in meters per second squared):
+        #x,y,z = bno.read_gravity()
+        # Sleep for a second until the next reading.
+        print ("magx=",mx,"magy="my,"magz=",mz,"Gyx=",Gx,"Gyy=",Gy,"Gyz=",Gz,"accelx=",ax,"accely=",ay,"accelz=",az) 
+        time.sleep(1)
+        now = datetime.datetime.now()
+        recordtime = 'rec_{0:%Y%m%d}'.format(now)
+        print("recordtime = %s" % recordtime)
+
+        data = "magx="+str(mx)+"magy="+str(my)+"magz="+str(mz)+"Gyx="+str(Gx)+"Gyy="+str(Gy)+"Gyz="+str(Gz)+"accelx="+str(ax)+"accely="+str(ay)+"accelz="+str(az)
+        data = recordtime + "," + data
+        data = data.split(',') 
+        print("data= %s" % data)
+
+        writer = csv.writer(f)
+        writer.writerow(data)
+
+        f.close()
+        print("loggging done. see log.csv")
+#参考https://qiita.com/kmaepu/items/779ab8e45bfe96230224
