43736724e4e7eb444be1a4e492e4f2ecb3d511ef
takashin9 <91837495+takashin9@users.noreply.github.com>
2022-07-09 11:49:59 +0900

    Update hc-sr04_i2c_write.py

--- sensor_data/hc-sr04/hc-sr04_i2c_write.py
+++ sensor_data/hc-sr04/hc-sr04_i2c_write.py
@@ -1,50 +1,50 @@
- import csv
+import csv
 
- import datetime
+import datetime
 
- def reading(sensor):
+def reading(sensor):
     import time
     import RPi.GPIO as GPIO
     GPIO.setwarnings(False)
-     
+        
     GPIO.setmode(GPIO.BOARD)
     TRIG = 11
     ECHO = 13
-     
+        
     if sensor == 0:
         GPIO.setup(TRIG,GPIO.OUT)
         GPIO.setup(ECHO,GPIO.IN)
         GPIO.output(TRIG, GPIO.LOW)
         time.sleep(0.3)
-         
+            
         GPIO.output(TRIG, True)
         time.sleep(0.00001)
         GPIO.output(TRIG, False)
- 
+
         while GPIO.input(ECHO) == 0:
-          signaloff = time.time()
-         
+            signaloff = time.time()
+            
         while GPIO.input(ECHO) == 1:
-          signalon = time.time()
- 
+            signalon = time.time()
+
         timepassed = signalon - signaloff
         distance = timepassed * 17000
         return distance
         GPIO.cleanup()
     else:
         print ("Incorrect usonic() function varible.")
-         
- print (reading(0))
 
- data = distance
- data = recordtime + "," + data
- data = data.split(',') 
- print("data= %s" % data)
 
- with open('log.csv', 'w') as f:   # log.csvのファイルに保存される
+
+with open('log.csv', 'w') as f:   # log.csvのファイルに保存される
     writer = csv.writer(f)
-    writer.writerow(data)
- f.close()
- print("loggging done. see log.csv")
+    while True:
+        data = reading(0)
+
+        print(data)
+        writer.writerow(data)
+
+        print("loggging done. see log.csv")
+
 
 #http://make.bcde.jp/raspberry-pi/%E8%B6%85%E9%9F%B3%E6%B3%A2%E8%B7%9D%E9%9B%A2%E3%82%BB%E3%83%B3%E3%82%B5hc-sr04%E3%82%92%E4%BD%BF%E3%81%86/
