ff41caa015ea3c8d6be8e314e86a2e3d3da362e8
iSSAtAKAHASHI <105526762+iSSAtAKAHASHI@users.noreply.github.com>
2022-07-16 18:26:32 +0900

    Update and rename PS4+motor.py to PS4+Motor(ver2).py

--- sensor_data/DRV8835/PS4+Motor(ver2).py
+++ sensor_data/DRV8835/PS4+Motor(ver2).py
@@ -1,8 +1,8 @@
 
-import RPi.GPIO as GPIO #GPIO用
-import time
+import RPi.GPIO as GPIO #GPIOインポート
+import time#時間制御インポート
 
-#PIN指定
+#PIN指定（物理番号指定）
 AIN1 = 15
 AIN2 = 29
 BIN1 = 31
@@ -10,35 +10,41 @@ BIN2 = 33
 
 #GPIOのモード
 GPIO.setmode(GPIO.BOARD)    #物理ピン番号でGPIOを指定
-GPIO.setup(AIN1,GPIO.OUT)   #a1ばんピンでGPIO
-GPIO.setup(AIN2,GPIO.OUT)   #a2ばんピンでGPIO(PWM)
-GPIO.setup(BIN1,GPIO.OUT)   #b1ばんピンでGPIO
-GPIO.setup(AIN2,GPIO.OUT)   #b2ばんピンでGPIO(PWM)
+GPIO.setup(AIN1,GPIO.OUT)   #AIN1番ピンで
+GPIO.setup(AIN2,GPIO.OUT)   #AIN2ピンで
+GPIO.setup(BIN1,GPIO.OUT)   #BIN1ピンで
+GPIO.setup(AIN2,GPIO.OUT)   #BIN2ピンでGPIO出力設定
 
 #周波数設定
-Apwm = GPIO.PWM(AIN2,255)     #a1で100HzPWM
-Bpwm = GPIO.PWM(BIN2,255)     #b1で100HzPWM
+Apwm = GPIO.PWM(AIN2,255)     #PWM(pin,Hz)→AIN2番ピンで255Hz
+Bpwm = GPIO.PWM(BIN2,255)     #PWM(pin,Hz)→BIN2番ピンで255Hz出力設定
 
-def forward():                #前進
-    Apwm.start(0)             #右車輪(1,0) 
+def forward():        #前進
+    #右車輪(A1,A2)=(+,0)→正回転
+    Apwm.start(0)               #start(duty比)つまり出力0の状態              
+    GPIO.output(AIN1,GPIO.HIGH) #AIN1を出力(pin,出力を100％) 
+    Apwm.ChangeDutyCycle(0)     #ChangeDutyCycle(duty)→AIN2のduty比０に設定
+    #左車輪(BIN1.BIN2)=(+,0)→正回転
+    Bpwm.start(0)               #
+    GPIO.output(BIN1,GPIO.HIGH) #def forward右車輪と同上
+    Bpwm.ChangeDutyCycle(0)     #
+    
+def turn_right():    #右回転
+    #右車輪(AIN1,AIN2)=(0,+)→負回転
+    Apwm.start(0)               # 
+    GPIO.output(AIN1,GPIO.LOW)  #def forward右車輪と同上
+    Apwm.ChangeDutyCycle(100)   #duty比100％→255Hz（max）出力
+    #左車輪(BIN1.BIN2)=(+,0)→正回転
+    Bpwm.start(0)               #    
+    GPIO.output(BIN1,GPIO.HIGH) #def forward右車輪と同上
+    Bpwm.ChangeDutyCycle(0)     #
+    
+def turn_left():     #左回転
+    #右車輪(AIN1.AIN2)=(+,0)→正回転
+    Apwm.start(0)                  
     GPIO.output(AIN1,GPIO.HIGH)
     Apwm.ChangeDutyCycle(0)
-    Bpwm.start(0)             #左車輪(1,0)
-    GPIO.output(BIN1,GPIO.HIGH)
-    Bpwm.ChangeDutyCycle(0)
-    
-def turn_right():                   #右回転
-    Apwm.start(0)                  #右車輪(0,1)
-    GPIO.output(AIN1,GPIO.LOW)
-    Apwm.ChangeDutyCycle(100)
-    Bpwm.start(0)                  #左車輪(1,0)
-    GPIO.output(BIN1,GPIO.HIGH)
-    Bpwm.ChangeDutyCycle(0)
-    
-def turn_left():                   #左回転
-    Apwm.start(0)                  #右車輪(1,0)
-    GPIO.output(AIN1,GPIO.HIGH)
-    Apwm.ChangeDutyCycle(0)
+    #左車輪(BIN1.BIN2)=(0,+)→正回転
     Bpwm.start(0)                  #左車輪(0,1)
     GPIO.output(BIN1,GPIO.LOW)
     Bpwm.ChangeDutyCycle(100)
